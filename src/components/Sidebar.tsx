2aaeb467b17f87cae7c61a78717168b998629a13
Daniel <danielrgb2@gmail.com>
2022-11-02 16:11:05 -0300

    Feat:pagina: dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,11 +9,11 @@ export function Sidebar() {
     <Box>
      <Text fontWeight='bold' color='gray.400' fontSize='small'>Geral</Text>
      <Stack spacing='4' mt='8' align='stretch'>
-      <Link display='flex'>
+      <Link display='flex' alignItems='center'>
        <Icon as={RiDashboardLine} fontSize='20' />
        <Text ml='4' fontWeight='medium'>Dashboard</Text>
       </Link>
-      <Link display='flex'>
+      <Link display='flex' alignItems='center'>
        <Icon as={RiContactsLine} fontSize='20' />
        <Text ml='4' fontWeight='medium'>Usuarios</Text>
       </Link>
