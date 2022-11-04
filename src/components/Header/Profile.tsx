e86dc0f6e9f54a00456819f56cd5da7c28076f48
Daniel <danielrgb2@gmail.com>
2022-11-04 15:49:28 -0300

    Feat:header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface profileProps {
+   showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: profileProps) {
  return (
   <Flex align="center">
-     <Box mr='4' textAlign='right'>
+     { showProfileData && (
+      <Box mr='4' textAlign='right'>
       <Text>Daniel Augusto</Text>
       <Text color='gray.300' fontSize='small'>danielrgb2@gmail.com</Text>
      </Box>
-
+     )}
      <Avatar size='md' name='Daniel Augusto' src='https://github.com/danielrgb23.png'/>
     </Flex>
  );
