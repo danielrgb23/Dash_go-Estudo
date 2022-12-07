400bdc6328c5625a4e64c43720710086756a95ef
Daniel <danielrgb2@gmail.com>
2022-12-07 18:02:23 -0300

    Feat:react form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: String;
+  password: String;
+}
+
 export default function Signin() {
+
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState
+
+  const handleSignin: SubmitHandler<SignInFormData > = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
+
   return (
     <Flex
       w="100vw"
@@ -17,15 +35,20 @@ export default function Signin() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignin)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" placeholder="E-mail"/>
+          <Input type="email" label="E-mail" placeholder="E-mail" {...register('email')} />
 
-          <Input name="password" type="password" label="Senha" placeholder="Senha"/>
+          <Input type="password" label="Senha" placeholder="Senha" {...register('password')} />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>Entrar</Button>
       </Flex>
     </Flex>
   )
 }
+function yupResolver(SignInFormSchema: any): import("react-hook-form").Resolver<SignInFormData, any> | undefined {
+  throw new Error('Function not implemented.');
+}
+
